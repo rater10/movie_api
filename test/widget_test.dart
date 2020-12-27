9c696cef9472e1e5086a439328c1217a1efec01c
Darius Gradinariu <gradinariudarius14@gmail.com>
2020-12-27 18:54:49 +0200

    add github actions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,9 @@ import 'package:movie_api/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(store: null,));
+    await tester.pumpWidget(const MyApp(
+      store: null,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
